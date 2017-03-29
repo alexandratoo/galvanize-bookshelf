7295064476d521939d778ddc4e5a802f4de78744
Alexandra Tooley <sandratooley@rocketmail.com>
2017-03-29 13:27:10 -0600

    part3

---
+++ migrations/20170329111121_users.js
@@ -0,0 +1,19 @@
+
+exports.up = function(knex) {
+  return knex.schema.createTable('users', (table) => {
+     table.increments();
+     table.string('first_name', 255).notNullable().defaultTo('');
+    table.string('last_name', 255).notNullable().defaultTo('');
+     table.string('email').unique().notNullable();
+     table.specificType('hashed_password', 'char(60)').notNullable();
+     table.timestamps(true, true);
+  });
+
+
+};
+
+
+  exports.down = function(knex) {
+     return knex.schema.dropTable('users');
+
+};
--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,24 @@ const express = require('express');
 
 // eslint-disable-next-line new-cap
 const router = express.Router();
-
+const bcrypt = require('bcrypt-as-promised');
+const knex = require('../knex');
 // YOUR CODE HERE
-
+router.post('/users', (req, res, next) => {
+  bcrypt.hash(req.body.password, 12)
+    .then((hashed_password) => {
+      return knex('users')
+        .insert({
+          hashed_password: hashed_password
+        }, '*');
+    })
+    .then((users) => {
+      const user = users[0];
+      delete user.hashed_password;
+      res.send(user);
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
 module.exports = router;
