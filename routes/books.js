71bc8ae09a12f8d23de91693ed0cb5458e60b9bf
Alexandra Tooley <sandratooley@rocketmail.com>
2017-03-29 07:54:05 -0600

    bookshelf2

--- routes/books.js
+++ routes/books.js
@@ -1,10 +1,58 @@
 'use strict';
 
 const express = require('express');
+const knex = require('../knex');
 
 // eslint-disable-next-line new-cap
 const router = express.Router();
 
 // YOUR CODE HERE
+router.get('/books', function(req, res){
+    knex('books')
+      .orderBy('title')
+       .then(function(data){
+          res.send(humps.camelizeKeys(data))
+      })
+ })
+ router.get('/books/:id', function(req, res){
+    knex('books')
+       .orderBy('title')
+       .where('id', req.params.id)
+       .then(function(data){
+          res.send(humps.camelizeKeys(data[0]))
+       })
+ })
+ router.post('/books', function(req, res){
+    knex('books')
+    .returning(['id', 'title', 'author', 'genre', 'description', 'cover_url'])
+    .insert({
+      'title': req.body.title,
+      'author': req.body.author,
+      'genre': req.body.genre,
+      'description': req.body.description,
+      'cover_url': req.body.coverUrl
+   })
 
-module.exports = router;
+    .then(function(data){
+       res.send(humps.camelizeKeys(data[0]));
+    })
+ })
+
+ router.patch('/books/:id', (req, res, next) => {
+    let id = req.params.id
+    knex('books')
+       .where('id', id)
+      .returning('*')
+       .update({
+         'title': req.body.title,
+         'author': req.body.author,
+         'genre': req.body.genre,
+         'description': req.body.description,
+         'cover_url': req.body.coverUrl
+      })
+       .then(function (data){
+          res.send(humps.camelizeKeys(data[0]));
+      })
+ });
+
+  module.exports = router;
