6bc3bf11338a85e0e65cda8714f5fc210c3ca906
Alexandra Tooley <sandratooley@rocketmail.com>
2017-03-30 08:55:21 -0600

    part3complete

--- routes/users.js
+++ routes/users.js
@@ -4,24 +4,23 @@ const express = require('express');
 
 // eslint-disable-next-line new-cap
 const router = express.Router();
-const bcrypt = require('bcrypt-as-promised');
+const bcrypt = require('bcrypt');
 const knex = require('../knex');
+const humps = require('humps')
 // YOUR CODE HERE
-router.post('/users', (req, res, next) => {
-  bcrypt.hash(req.body.password, 12)
-    .then((hashed_password) => {
-      return knex('users')
-        .insert({
-          hashed_password: hashed_password
-        }, '*');
-    })
-    .then((users) => {
-      const user = users[0];
-      delete user.hashed_password;
-      res.send(user);
-    })
-    .catch((err) => {
-      next(err);
-    });
-});
+router.post('/users', (req, res) => {
+      knex('users')
+      .returning(['id', 'email', 'first_name', 'last_name'])
+      .insert({
+        'email': req.body.email,
+        'first_name': req.body.firstName,
+        'last_name': req.body.lastName,
+        'hashed_password': bcrypt.hashSync(req.body.password, 8)
+     })
+      .then((data) => {
+         res.send(humps.camelizeKeys(data[0]))
+
+      })
+})
+
 module.exports = router;
