48533f7a07b0b9af31298675b44de433b0e124e7
Alexandra Tooley <sandratooley@rocketmail.com>
2017-04-20 11:48:17 -0600

    authors

--- knexfile.js
+++ knexfile.js
@@ -2,7 +2,7 @@
 module.exports = {
   development: {
     client: 'pg',
-    connection: 'postgres://localhost/galvanize-bookshelf'},
+    connection: 'postgres://localhost/bookshelf_dev'},
 
   test: {
     client: 'pg',
--- seeds/3_favorites.js
+++ seeds/3_favorites.js
@@ -1,28 +1,19 @@
-
-'use strict';
-
-const express = require('express');
-
-// eslint-disable-next-line new-cap
-const router = express.Router();
-
-// YOUR CODE HERE
 exports.seed = function(knex) {
-return knex('favorites').del()
+  // Deletes ALL existing entries
+  return knex('favorites').del()
     .then(function () {
       return Promise.all([
         knex('favorites').insert([{
-            id: 1,
+           id: 1,
            book_id: 1,
-            user_id: 1,
+           user_id: 1,
            created_at: new Date('2016-06-29 14:26:16 UTC'),
-            updated_at: new Date('2016-06-29 14:26:16 UTC')
-          }]),
-       ]);
+           updated_at: new Date('2016-06-29 14:26:16 UTC')
+         }]),
+      ]);
     })
-     .then(() => {
-       return knex.raw("SELECT setval('favorites_id_seq', (SELECT MAX(id) FROM favorites));")
+    .then(() => {
+      return knex.raw("SELECT setval('favorites_id_seq', (SELECT MAX(id) FROM favorites));")
     })
-     ;
- };
-module.exports = router;
+    ;
+};
